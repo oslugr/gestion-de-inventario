05f040d7e7c0c094622818d707422eaa5b2b8655
Pablo <pablocorderoromero@gmail.com>
2020-11-27 09:20:26 +0100

    Gestión de las localizaciones en la base de datos

--- routes/localizacion.js
+++ routes/localizacion.js
@@ -4,7 +4,7 @@
 var express = require('express'),
 	router = express.Router();
 const db = require('../db/pool').pool;
-const { validate, ValidationError, Joi } = require('express-validation');
+const { body, validationResult } = require('express-validator');
 
 // Obtiene todas las localizaciones de la DB
 router.get('/', (req, res) => {
@@ -15,18 +15,22 @@ router.get('/', (req, res) => {
 				
 				conn.release();
 				if(!err)
-					res.json(rows);
-				else
-					console.log("Error al hacer el query: SELECT * FROM localizacion");
-			
+					res.status('200').json(rows);
+				else{
+					console.log("Error al obtener localizaciones. Query: SELECT * FROM localizacion");
+					return res.status('502').json({
+						tipo_error: 502,
+						mensaje_error: "Bad Gateway - Error interno al obtener los valores de la base de datos"
+					})
+				}
 			})
 		}
 		else{
-			console.log("Erro en la conexión con la base de datos");
+			console.log("Error en la conexión con la base de datos");
 		}
 	})
 
-})
+});
 
 /**
  * Añade una localización a la base de datos
@@ -34,13 +38,51 @@ router.get('/', (req, res) => {
  * {
  * 		"nombre": ""
  * }
- * 
  */
-// Formato de introducción
-router.post('/', (req, res) => {
+router.post('/',[ 
+	// Comprobación de parámetros
+	body('nombre').not().isEmpty().withMessage('Nombre no introducido')
+], (req, res) => {
 
-	console.log(req.body)
-	res.json(req.body);
+	// Gestión de errores 
+	const errors = validationResult(req);
+	if(!errors.isEmpty())
+		return res.status(400).json({
+			tipo_error: 400,
+			mensaje_error: "Bad request",
+			errores: errors.array()
+		})
+
+	// Si no hay error añade la localizacion en la base de datos
+	let nombre = req.body.nombre;
+	db.getConnection(function(err, conn){
+		if(!err){
+			conn.query(`INSERT INTO localizacion VALUES ("${nombre}")`, function(err, rows){
+				
+				conn.release();
+				if(!err){
+					return res.status('200').json({
+						codigo: "200",
+						mensaje: "Localización insertada correctamente"
+					});
+				}
+				else{
+					console.log(`Error al insertar una localización en la base de datos. Query: INSERT INTO localizacion VALUES ${nombre}`);
+					return res.status('502').json({
+						tipo_error: 502,
+						mensaje_error: "Bad Gateway - Error interno al insertar valores a la base de datos"
+					})
+				}
+			})
+		}
+		else{
+			console.log("Error en la conexión con la base de datos");
+			return res.status('503').json({
+				tipo_error: 503,
+				mensaje_error: "Service Unavailable - Error interno al conectarse a la base de datos"
+			})
+		}
+	})
 
 })
 
