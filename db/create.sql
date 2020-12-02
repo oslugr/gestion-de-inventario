57399f0aa580e5afad27446ffe049bff46719235
Pablo <pablocorderoromero@gmail.com>
2020-12-02 13:05:28 +0100

    Creación de la base de datos completa

--- db/create.sql
+++ db/create.sql
@@ -1,21 +1,136 @@
 CREATE TABLE localizacion(
-	nombre VARCHAR(40) PRIMARY KEY
+	nombre VARCHAR(120) PRIMARY KEY
+);
+
+CREATE TABLE cable(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	tipo VARCHAR(120),
+	version_tipo VARCHAR(20)
 );
 
 CREATE TABLE recogida(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	localizacion VARCHAR(40) REFERENCES localizacion(nombre),
-	fecha DATETIME,
-    UNIQUE(localizacion, fecha)
+	fecha DATETIME
+);
+
+CREATE TABLE en(
+	localizacion VARCHAR(120) REFERENCES localizacion(nombre),
+	id_recogida INT REFERENCES recogida(id),
+	PRIMARY KEY (localizacion, id_recogida)
 );
 
 CREATE TABLE componente (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    estado ENUM('Desconocido', 'Bueno', 'Por revisar', 'No aprovechable', 'Roto') DEFAULT 'Desconocido'
+    estado ENUM('Desconocido', 'Bueno', 'Regular', 'Por revisar', 'No aprovechable', 'Roto') DEFAULT 'Desconocido',
+	observaciones VARCHAR(1000)
 );
 
-CREATE TABLE componente_recogida(
+CREATE TABLE ordenador (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	localizacion_taller VARCHAR(30),
+	observaciones VARCHAR(1000)
+);
+
+CREATE TABLE portatil (
+	id INT REFERENCES ordenador(id),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE transformador (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	voltaje FLOAT,
+	amperaje FLOAT
+);
+
+CREATE TABLE corresponde_portatil (
+	id_transformador INT REFERENCES transformador(id),
+	id_portatil INT REFERENCES portatil(id),
+	PRIMARY KEY(id_transformador, id_portatil)
+);
+
+CREATE TABLE corresponde_componente (
+	id_transformador INT REFERENCES transformador(id),
+	id_componente INT REFERENCES componente(id),
+	PRIMARY KEY(id_transformador, id_componente)
+);
+
+CREATE TABLE sobremesa (
+	id INT REFERENCES ordenador(id),
+	tamano VARCHAR(40),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE formado (
+	id_componente INT REFERENCES componente(id),
+	id_ordenador INT REFERENCES ordenador(id),
+	PRIMARY KEY(id_ordenador, id_componente),
+    UNIQUE(id_componente)
+);
+
+CREATE TABLE contiene_cable(
+	id_cable INT REFERENCES cable(id),
 	id_recogida INT REFERENCES recogida(id),
-    id_componente INT REFERENCES componente(id),
-    PRIMARY KEY(id_recogida, id_componente)
+	PRIMARY KEY(id_cable, id_recogida),
+    UNIQUE(id_cable)
+);
+
+CREATE TABLE contiene_ordenador(
+	id_ordenador INT REFERENCES ordenador(id),
+	id_recogida INT REFERENCES recogida(id),
+	PRIMARY KEY(id_ordenador, id_recogida),
+    UNIQUE(id_ordenador)
+);
+
+CREATE TABLE contiene_componente(
+	id_componente INT REFERENCES componente(id),
+	id_recogida INT REFERENCES recogida(id),
+	PRIMARY KEY(id_componente, id_recogida),
+    UNIQUE(id_componente)
+);
+
+CREATE TABLE contiene_transformador(
+	id_transformador INT REFERENCES transformador(id),
+	id_recogida INT REFERENCES recogida(id),
+	PRIMARY KEY(id_transformador, id_recogida),
+    UNIQUE(id_transformador)
+);
+
+CREATE TABLE impresora (
+	id INT PRIMARY KEY REFERENCES componente(id),
+	tinta BOOLEAN,
+	escaner BOOLEAN
+);
+
+CREATE TABLE escaner(
+	id INT PRIMARY KEY REFERENCES componente(id)
+);
+
+CREATE TABLE teclado(
+	id INT PRIMARY KEY REFERENCES componente(id)
+);
+
+CREATE TABLE raton(
+	id INT PRIMARY KEY REFERENCES componente(id)
+);
+
+CREATE TABLE monitor(
+	id INT PRIMARY KEY REFERENCES componente(id),
+	resolucion VARCHAR(15)
+);
+
+CREATE TABLE disco(
+	id INT PRIMARY KEY REFERENCES componente(id),
+	capacidad FLOAT,
+	tipo ENUM('SSD', 'HDD'),
+	conector VARCHAR(20)
+);
+
+CREATE TABLE placa_base(
+	id INT PRIMARY KEY REFERENCES componente(id)
+);
+
+CREATE TABLE ram(
+	id INT PRIMARY KEY REFERENCES componente(id),
+	capacidad FLOAT,
+	tipo VARCHAR(20)
 );
