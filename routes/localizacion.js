a44ec2d60e8712d20138c56d5886b3d253463597
Pablo <pablocorderoromero@gmail.com>
2021-01-08 08:58:56 +0100

    Pequeña explicación de que hace cada ruta de localizaciones

--- routes/localizacion.js
+++ routes/localizacion.js
@@ -5,7 +5,9 @@ const controlador = require('../controllers/localizacion');
 
 // Obtiene todas las localizaciones de la DB
 router.get	  ('/',         controlador.obtenerLocalizaciones);
+// Añade una localización con el nombre especificado
 router.post	  ('/:nombre',  controlador.insertarLocalizacion);
+// Elimina la localización con el nombre especificado si esta existe
 router.delete ('/:nombre',  controlador.eliminarLocalizacion)
 
 module.exports = router;
