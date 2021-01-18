497d99dd983b3401bef5671393cbc26b8f793269
Pablo <pablocorderoromero@gmail.com>
2021-01-18 13:58:08 +0100

    Cambiada la ruta de la api

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const cable         = require('./routes/cable')
 const transformador = require('./routes/transformador')
 const componente    = require('./routes/componente')
 const recogida      = require('./routes/recogida')
-const ordenador      = require('./routes/ordenador')
+const ordenador     = require('./routes/ordenador')
 
 // Para poder recibir peticiones con cuerpo JSON
 app.use(express.json())
-app.use('/localizacion', localizacion);
-app.use('/cable', cable);
-app.use('/transformador', transformador);
-app.use('/componente', componente);
-app.use('/recogida', recogida);
-app.use('/ordenador', ordenador);
+app.use('/api/localizacion', localizacion);
+app.use('/api/cable', cable);
+app.use('/api/transformador', transformador);
+app.use('/api/componente', componente);
+app.use('/api/recogida', recogida);
+app.use('/api/ordenador', ordenador);
 
 app.use('/', express.static('public'));
 
