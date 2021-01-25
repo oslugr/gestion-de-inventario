f436c321b4f6292fdc20ac93b10e4c2cdf3ee586
Pablo <pablocorderoromero@gmail.com>
2021-01-25 14:00:28 +0100

    Eliminar cables desde la tabla funcionando en el front-end

--- public/assets/js/contenido/cables.js
+++ public/assets/js/contenido/cables.js
@@ -20,7 +20,7 @@ function tarjeta(titulo, dato) {
 				<p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
 				${titulo}
 				</p>
-				<p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
+				<p id="elementos-totales-tarjeta" class="text-lg font-semibold text-gray-700 dark:text-gray-200">
 				${dato}
 				</p>
 			</div>
@@ -48,11 +48,11 @@ let cables = null;
 let numElementosActuales = 0;
 let paginaMax = 0;
 
-function fila(id, tipo, version) {
+function fila(id, tipo, version, posicion) {
 	if (version == null) version = "";
 
 	return `
-		<tr class="cable text-gray-700 dark:text-gray-400">
+		<tr id="cable-${id}" class="cable text-gray-700 dark:text-gray-400">
 			<td class="px-4 py-3">
 				<div class="flex items-center text-sm">
 					<svg class="relative hidden w-6 h-6 mr-3 md:block" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" ><path d="M20 12H4" ></path></svg>
@@ -74,7 +74,7 @@ function fila(id, tipo, version) {
 							<path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
 						</svg>
 					</button>
-					<button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
+					<button onclick="eliminarCable(${id}, ${posicion});" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
 						<svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
 							<path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
 						</svg>
@@ -93,7 +93,7 @@ function crearCables() {
 	$('.cable').remove();
 
 	for (i = 0; i < elementos.length; i++)
-		filas += fila(elementos[i].id, elementos[i].tipo, elementos[i].version_tipo);
+		filas += fila(elementos[i].id, elementos[i].tipo, elementos[i].version_tipo, (pagina - 1)*10+i );
 
 	$("#body-tabla").append(filas);
 
@@ -238,10 +238,12 @@ function generarNavegadorTabla(){
 }
 
 function generarPie() {
+	$('#pie-tabla').remove();
+
 	$("#main-tabla").append(`
-		<div class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
+		<div id="pie-tabla" class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
 			<span id="cantidad-pie" class="flex items-center col-span-3">
-				Mostrando ${numElementosActuales} de ${cables.cantidad}
+				Mostrando ${numElementosActuales} de ${cables.data.length}
 			</span>
 			<span class="col-span-2"></span>
 			<!-- Pagination -->
@@ -257,6 +259,17 @@ function generarPie() {
 	  generarNavegadorTabla();
 }
 
-function eliminarCable(tipo, version){
-	$.delete("/api/cable/", crearInterfazCables);
+function eliminarCable(id, posicion){
+	$.ajax({
+		url: `/api/cable/id/${id}`,
+		type: 'DELETE',
+		success: function(){
+			cables.data.splice(posicion,1);
+			crearCables();
+			generarPie();
+
+			// Actualiza la tarjeta superior
+			$('#elementos-totales-tarjeta').html(cables.data.length);
+		}
+	});
 }
