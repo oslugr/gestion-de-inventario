45dece3c55f157969a554eeea0630cc47bd7f6d0
Pablo <pablocorderoromero@gmail.com>
2021-01-26 09:42:15 +0100

    Edición de cables funcionando en el front-end

--- public/assets/js/contenido/cables.js
+++ public/assets/js/contenido/cables.js
@@ -61,15 +61,15 @@ function fila(id, tipo, version, posicion) {
 					</div>
 				</div>
 			</td>
-			<td class="px-4 py-3 text-sm">
+			<td id="tipo-${id}" class="px-4 py-3 text-sm">
 				${tipo}
 			</td>
-			<td class="px-4 py-3 text-xs">
+			<td id="version-${id}" class="px-4 py-3 text-xs">
 				${version}
 			</td>
 			<td class="px-4 py-3">
 				<div class="flex items-center space-x-4 text-sm">
-					<button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
+					<button id="editar-${id}" @click="openModal" onclick="cargarFormulario('${id}', '${tipo}', '${version}')" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
 						<svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
 							<path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
 						</svg>
@@ -272,4 +272,29 @@ function eliminarCable(id, posicion){
 			$('#elementos-totales-tarjeta').html(cables.data.length);
 		}
 	});
+}
+
+function cargarFormulario(id, tipo, version){
+
+	$('#editar-tipo').val(tipo);
+	$('#editar-version').val(version);
+	$('#confirmar-modal').attr('onclick', `editarCable(${id})`);
+
+}
+
+function editarCable(id){
+
+	let tipo 	= $('#editar-tipo').val();
+	let version = $('#editar-version').val();
+	
+	$.ajax({
+		url: `/api/cable/${id}/${tipo}/${version}`,
+		type: 'PUT',
+		success: function(){
+			$(`#tipo-${id}`).html(tipo);
+			$(`#version-${id}`).html(version);
+			$(`#editar-${id}`).attr('onclick', `cargarFormulario('${id}', '${tipo}', '${version}')`);
+		}
+	});
+
 }
