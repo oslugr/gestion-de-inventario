483292adc1b80b6dd62a2953d3072b1e5e0df827
Pablo <pablocorderoromero@gmail.com>
2021-02-01 12:50:28 +0100

    Pequeño error al gestionar las fechas

--- routes/componente.js
+++ routes/componente.js
@@ -11,7 +11,7 @@ router.get   ('/:tipo?',        controlador.obtenerComponentes);
 router.post  ('/',[ 
 	body('estado').optional().isIn(['Desconocido', 'Bueno', 'Regular', 'Por revisar', 'No aprovechable', 'Roto']).not().isEmpty().withMessage('Tipo no válido. Tiene que estar entre los siguientes valores: Desconocido, Bueno, Regular, Por revisar, No aprovechable, Roto'),
 	body('observaciones').optional().isString().withMessage('Valor no válido en observaciones'),
-	body('fecha_entra').optional().matches(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/).withMessage('Fecha mal introducida. Formatos de fecha correcto: AAAA-MM-DD'),
+	body('fecha_entrada').optional().matches(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/).withMessage('Fecha mal introducida. Formatos de fecha correcto: AAAA-MM-DD'),
 	body('tipo').not().isEmpty().withMessage('Tipo no introducido. Este parámetro es obligatorio'),
 	body('caracteristicas').optional().isArray().withMessage('Las características deben de ser un array'),
 	body('caracteristicas.*.nombre').isString().not().isEmpty().withMessage('Algún nombre de característica no es válido'),
@@ -34,7 +34,7 @@ router.put   ('/:id/estado/:estado',controlador.modificarEstado);
 router.put   ('/:id',[ 
 	body('estado').optional().isIn(['Desconocido', 'Bueno', 'Regular', 'Por revisar', 'No aprovechable', 'Roto']).not().isEmpty().withMessage('Tipo no válido. Tiene que estar entre los siguientes valores: Desconocido, Bueno, Regular, Por revisar, No aprovechable, Roto'),
 	body('observaciones').optional().isString().withMessage('Valor no válido en observaciones'),
-	body('fecha_entra').optional().matches(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/).withMessage('Fecha mal introducida. Formatos de fecha correcto: AAAA-MM-DD'),
+	body('fecha_entrada').matches(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/).withMessage('Fecha mal introducida. Formatos de fecha correcto: AAAA-MM-DD'),
 	body('tipo').not().isEmpty().withMessage('Tipo no introducido. Este parámetro es obligatorio')
 ],        						controlador.actualizarComponente);
 
