5b3eeb2f3b3e3c8c10a622497950a5f64f60cb49
Pablo <pablocorderoromero@gmail.com>
2021-02-22 14:00:32 +0100

    feat(frontend): ✨🚧 Añadir nuevas componentes
    Añadiendo nuevas componentes funcionando. Lo único que falta es que al añadir una componente no se obtienen la id de las características, por lo que posteriormente no se pueden editar

--- public/assets/js/contenido/componentes.js
+++ public/assets/js/contenido/componentes.js
@@ -22,6 +22,18 @@ function tarjeta(titulo, dato) {
 				</p>
 			</div>
     </div>
+	<div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 cursor-pointer" onclick="cargarFormularioVacio()" @click="openModal">
+			<div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
+				<svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+				</svg>
+			</div>
+			<div>
+				<p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+				Añadir componente
+				</p>
+			</div>
+    </div>
     `
 }
 
@@ -464,6 +476,36 @@ function aniadirCaracteristica(caracteristica, posicion_componente, posicion_car
 
 }
 
+function aniadirCaracteristicaNueva(num){
+	$('#body-tabla-caracteristicas').append(`
+		<tr id="fila-caracteristica-nueva-${num}" class="fila-caracteristica-nueva text-gray-700 dark:text-gray-400">
+			<td class="px-2 py-3 text-sm">
+				<input id="caracteristica-nueva-${num}-nombre" class="w-32 dark:border-gray-600 dark:bg-gray-800 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray" type="text" value=""/>
+			</td>
+			<td class="px-2 py-3 text-sm">
+				<input id="caracteristica-nueva-${num}-valor" class="w-32 dark:border-gray-600 dark:bg-gray-800 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray" type="text" value=""/>
+			</td>
+			<td class="px-2 py-3">
+			
+			<div class="flex items-center space-x-4 text-sm">
+				<button @click="openModal" id="icono-borrar-caracteristica-nueva-${num}" onclick="eliminarCaracteristicaNueva(${num});" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray">
+					<svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+						<path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+					</svg>
+				</button>
+			</div>
+			</td>
+		</tr>
+	`)
+
+	$('#boton-nueva-caracteristica').attr('onclick', `aniadirCaracteristicaNueva(${num+1})`);
+
+}
+
+function eliminarCaracteristicaNueva(num){
+	$(`#fila-caracteristica-nueva-${num}`).remove();
+}
+
 function cargarCaracteristicas(id, posicion){
 
 	let numeroComponentes = componentes.data[posicion].caracteristicas.length;
@@ -510,6 +552,71 @@ function cargarFormulario(id, tipo, estado, fecha, observaciones, posicion){
 	cargarCaracteristicas(id, posicion);
 	$('#confirmar-modal').attr('onclick', `editarComponente(${id}, ${posicion})`);
 	$('#boton-nueva-caracteristica').attr('onclick', `aniadirCaracteristica({id: -1, nombre: '', valor: ''}, ${posicion}, null, ${id}, 'crear');`);
+}
+
+function cargarFormularioVacio(){
+
+	$('#editar-tipo').val("");
+	$(`#editar-estado`).val("Desconocido");
+	$('#editar-fecha').val("");
+	$('#body-tabla-caracteristicas').html('');
+	$('#editar-observaciones').val("");
+	$('#confirmar-modal').attr('onclick', `crearComponente()`);
+	$('#boton-nueva-caracteristica').attr('onclick', `aniadirCaracteristicaNueva(0)`);
+
+}
+
+function crearComponente(){
+	const tipo 	        = $('#editar-tipo').val();
+	const estado        = $('#editar-estado').val();
+	const fecha         = $('#editar-fecha').val();
+	const observaciones = $('#editar-observaciones').val();
+	let caracteristicas = [];
+
+	const json = {
+		"tipo": tipo,
+		"estado": estado,
+		"observaciones": observaciones
+	};
+
+	if(fecha)
+		json["fecha_entrada"] = fecha;
+
+	let filas = document.querySelectorAll('.fila-caracteristica-nueva');
+	for (let i = 0; i < filas.length; i++) {
+		datos = filas[i].getElementsByTagName('input');
+		caracteristicas.push({
+			nombre: datos[0].value,
+			valor: datos[1].value
+		});
+	}
+
+	json["caracteristicas"] = caracteristicas;
+
+	$.ajax({
+		url: `/api/componente/`,
+		data: JSON.stringify(json),
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		type: 'POST',
+		success: function(data){
+
+			let id = data.id;
+			componentes.cantidad++;
+
+			componentes.data.push({
+				id: id,
+				tipo: tipo, 
+				estado: estado,
+				fecha_entrada: fecha,
+				observaciones: observaciones,
+				caracteristicas: caracteristicas
+			})
+
+			$('#elementos-totales-tarjeta').html(componentes.cantidad);
+			crearComponentes();
+		}
+	});
 }
 
 function editarComponente(id, posicion){
