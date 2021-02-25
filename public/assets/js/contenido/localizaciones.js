0b6c8c75ad94fea673b1382542f333d3c1f962fb
Pablo <pablocorderoromero@gmail.com>
2021-02-25 13:55:12 +0100

    feat(frontend): ✨ Gestión de localizaciones

---
+++ public/assets/js/contenido/localizaciones.js
@@ -0,0 +1,259 @@
+$.get("/api/localizacion", crearInterfazLocalizaciones);
+
+// -------------------------------------
+// Gestión de la tabla de localizaciones
+// -------------------------------------
+
+// Variable de estado para comprobar la página en la que se encuentra
+let pagina = 1;
+let localizaciones = null;
+let numElementosActuales = 0;
+let paginaMax = 0;
+
+function fila(nombre, posicion) {
+
+	return `
+		<tr id="localizacion-${nombre}" class="localizacion text-gray-700 dark:text-gray-400">
+			<td class="px-4 py-3">
+				<div class="flex items-center text-sm">
+					<svg class="relative hidden w-6 h-6 mr-3 md:block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                    <div>
+						<p class="font-semibold">${nombre}</p>
+					</div>
+				</div>
+			</td>
+			<td class="px-4 py-3">
+				<div class="flex items-center space-x-4 text-sm">
+					<button onclick="eliminarLocalizacion('${nombre}', ${posicion});" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
+						<svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+							<path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+						</svg>
+					</button>
+				</div>
+			</td>
+		</tr>
+  `;
+}
+
+function crearLocalizaciones() {
+	let filas = '';
+	let elementos = localizaciones.data.slice((pagina - 1) * 10, (pagina - 1) * 10 + 10);
+	paginaMax = Math.ceil(localizaciones.cantidad/10);
+
+	// Elimina la tabla de localizaciones
+	$('.localizacion').remove();
+
+	for (i = 0; i < elementos.length; i++)
+		filas += fila(elementos[i].nombre, (pagina - 1)*10+i );
+
+	$("#body-tabla").append(filas);
+
+	// Actualiza el pie de la tabla
+	if(numElementosActuales){
+		numElementosActuales = elementos.length;
+		$('#cantidad-pie').html(`Mostrando ${numElementosActuales} de ${localizaciones.cantidad}`);
+	}
+	else
+		numElementosActuales = elementos.length;
+
+	generarNavegadorTabla();
+}
+
+function crearTabla() {
+
+	$("#main-contenido").append(`
+		<div id="main-tabla" class="w-full overflow-hidden rounded-lg shadow-xs">
+
+			<div class="w-full overflow-x-auto">
+				<table class="w-full whitespace-no-wrap">
+					<thead>
+						<tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+							<th class="px-4 py-3">Nombre</th>
+							<th class="px-4 py-3">Acciones</th>
+						</tr>
+					</thead>
+					<tbody id="body-tabla" class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+					</tbody>
+				</table>
+			</div>
+	
+		</div>
+  `);
+
+	crearLocalizaciones();
+}
+
+function crearInterfazLocalizaciones(data) {
+	localizaciones = data;
+	paginaMax = Math.ceil(data.cantidad/10);
+
+	eliminaPantallaCargando();
+	if (localizaciones.cantidad) {
+		crearTabla();
+		generarPie();
+	}
+}
+
+function getPagina(num) {
+	pagina = num;
+	crearLocalizaciones();
+}
+
+function generarNavegadorTabla(){
+	let html = "";
+
+	// Elimina los actuales al cambiar de tabla
+	$('.elemento-navegacion-tabla').remove();
+
+	html += `
+		<li class="elemento-navegacion-tabla">
+			<button onclick="if(pagina>1) getPagina(pagina-1);" class="px-3 py-1 rounded-md rounded-l-lg focus:outline-none focus:shadow-outline-purple" aria-label="Previous">
+				<svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
+					<path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" fill-rule="evenodd"></path>
+				</svg>
+			</button>
+		</li>
+	`;
+
+	if(paginaMax > 5){
+
+		if(pagina != 1){
+			html += `<li class="elemento-navegacion-tabla"> 
+						<button onclick="getPagina(1);" class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+							1 
+						</button>
+					</li>
+					`
+		}
+
+		if(pagina >= 3){
+			html += `<li class="elemento-navegacion-tabla">
+						<span class="px-3 py-1">...</span>
+					</li>
+					`
+		}
+
+		html += `<li class="elemento-navegacion-tabla">
+					<button onclick="getPagina(${pagina});" class="px-3 py-1 text-white transition-colors duration-150 bg-purple-600 border border-r-0 border-purple-600 rounded-md focus:outline-none focus:shadow-outline-purple">
+						${pagina}
+					</button>
+				</li>
+				`
+
+		if(pagina != paginaMax){
+			if(pagina<paginaMax-1){
+				html += `<li class="elemento-navegacion-tabla">
+							<span class="px-3 py-1">...</span> 
+						</li>
+						`
+			}
+
+			html += `
+				<li class="elemento-navegacion-tabla">
+					<button onclick="getPagina(${paginaMax});" class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+						${paginaMax}
+					</button>
+				</li>
+			`
+		}
+	}
+	else{
+
+		for (i = 1; i <= paginaMax; i++) {
+			html += `
+				<li class="elemento-navegacion-tabla">
+					<button onclick="getPagina(${i});" 
+						${ pagina!=i ? 'class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple"' : 'class="px-3 py-1 text-white transition-colors duration-150 bg-purple-600 border border-r-0 border-purple-600 rounded-md focus:outline-none focus:shadow-outline-purple"' } >
+						${i}
+					</button>
+				</li>
+			`
+		}
+
+	}
+
+	html += `
+		<li class="elemento-navegacion-tabla">
+			<button onclick="if(pagina<paginaMax) getPagina(pagina+1);" class="px-3 py-1 rounded-md rounded-r-lg focus:outline-none focus:shadow-outline-purple" aria-label="Next">
+				<svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
+					<path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" fill-rule="evenodd"></path>
+				</svg>
+			</button>
+		</li>
+	`;
+	
+	$('#navegacion-tablas').html(html);
+}
+
+function generarPie() {
+	$('#pie-tabla').remove();
+
+	$("#main-tabla").append(`
+		<div id="pie-tabla" class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
+			<span id="cantidad-pie" class="flex items-center col-span-3">
+				Mostrando ${numElementosActuales} de ${localizaciones.cantidad}
+			</span>
+			<span class="col-span-2"></span>
+			<!-- Pagination -->
+			<span class="flex col-span-4 mt-2 sm:mt-auto sm:justify-end">
+				<nav aria-label="Table navigation">
+					<ul id="navegacion-tablas" class="inline-flex items-center">
+					</ul>
+				</nav>
+			</span>
+		</div>
+	  `);
+	  
+	  generarNavegadorTabla();
+}
+
+function eliminarLocalizacion(nombre, posicion){
+	$.ajax({
+		url: `/api/localizacion/${nombre}`,
+		type: 'DELETE',
+		success: function(){
+			localizaciones.cantidad--;
+			
+			if(localizaciones.cantidad%10 == 0){
+				pagina--;
+			}
+
+			localizaciones.data.splice(posicion,1);
+			crearLocalizaciones();
+			generarPie();
+
+			// Actualiza la tarjeta superior
+			$('#elementos-totales-tarjeta').html(localizaciones.cantidad);
+		}
+	});
+}
+
+function cargarFormularioVacio(){
+
+	$('#editar-nombre').val('');
+	$('#confirmar-modal').attr('onclick', `crearLocalizacion()`);
+}
+
+function crearLocalizacion(){
+
+	let nombre = $('#editar-nombre').val();
+	
+	$.ajax({
+		url: `/api/localizacion/${nombre}`,
+		type: 'POST',
+		success: function(data){
+			localizaciones.cantidad++;
+
+			localizaciones.data.push({
+				nombre: nombre
+			})
+
+			$('#elementos-totales-tarjeta').html(localizaciones.cantidad);
+			crearLocalizaciones();
+		}
+	});
+
+}
