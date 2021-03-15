1d326954186a8a9c159b7d3d97d03464d8ebed02
Pablo Cordero <pablocorderoromero@gmail.com>
2021-03-15 20:13:55 +0100

    feat(frontend): ✨ Se muestran componentes en la recogida
    Ahora se muestran los componentes en la recogida

--- public/assets/js/contenido/recogida.js
+++ public/assets/js/contenido/recogida.js
@@ -20,6 +20,7 @@ function crearInterfazRecogida(data){
     // Crear Transformadores
     $.get(`/api/recogida/${recogida.id}/transformador`, crearInterfazTransformadores);
     // Crear componentes
+    $.get(`/api/recogida/${recogida.id}/componente`, crearInterfazComponentes);
 
     // crearInterfazComponentes(ordenador.componentes);
 }
@@ -1001,3 +1002,498 @@ function crearTransformador(){
 	});
 
 }
+
+// ----------------------------------------------------------------------------------
+// COMPONENTES
+// ----------------------------------------------------------------------------------
+
+// Variable de estado para comprobar la página en la que se encuentra
+let pagina_componentes = 1;
+let componentes = null;
+let numElementosActuales_componentes = 0;
+let paginaMax_componentes = 0;
+
+function fila_componentes(id, estado, fecha, tipo, observaciones, posicion) {
+	if (observaciones == null) observaciones = "";
+	if (fecha == null) 		   fecha = "";
+
+	return `
+		<tr id="componente-${id}" class="componente text-gray-700 dark:text-gray-400">
+			<td class="px-4 py-3">
+				<div class="flex items-center text-sm">
+					<svg class="relative hidden w-6 h-6 mr-3 md:block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" /></svg>
+					<div>
+						<p class="font-semibold">${id}</p>
+					</div>
+				</div>
+			</td>
+			<td id="componente_tipo-${id}" class="px-4 py-3 text-sm">
+				${tipo}
+			</td>
+			<td id="componente_estado-${id}" class="px-4 py-3 text-sm">
+				${obtenerEstadoFormateado(estado)}
+			</td>
+			<td id="componente_fecha-${id}" class="px-4 py-3 text-sm">
+				${fecha.split('T')[0]}
+			</td>
+			<td id="componente_observaciones-${id}" class="px-4 py-3 text-xs">
+				${observaciones}
+			</td>
+			<td class="px-4 py-3">
+				<div class="flex items-center space-x-4 text-sm">
+					<button id="editar-${id}" @click="openModal" onclick="cargarFormularioComponente('${id}', '${tipo}', '${estado}', '${fecha.split('T')[0]}', '${observaciones}', '${posicion}' )" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
+						<svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+							<path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
+						</svg>
+					</button>
+					<button onclick="eliminarComponente(${id}, ${posicion});" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
+						<svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+							<path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+						</svg>
+					</button>
+				</div>
+			</td>
+		</tr>
+  `;
+}
+
+
+function crearComponentes() {
+	let filas = '';
+	let elementos = componentes.data.slice((componentes - 1) * 10, (pagina_componentes - 1) * 10 + 10);
+	paginaMax_componentes = Math.ceil(componentes.cantidad/10);
+
+	// Elimina la tabla de componentes
+	$('.componentes').remove();
+
+	for (i = 0; i < elementos.length; i++)
+		filas += fila_componentes(elementos[i].id, elementos[i].estado, elementos[i].fecha_entrada, elementos[i].tipo, elementos[i].observaciones, (pagina_componentes - 1)*10+i );
+
+	$("#body-tabla-componentes").append(filas);
+
+	// Actualiza el pie de la tabla
+	if(numElementosActuales_componentes){
+		numElementosActuales_componentes = elementos.length;
+		$('#cantidad-pie_componentes').html(`Mostrando ${numElementosActuales_componentes} de ${componentes.cantidad}`);
+	}
+	else
+		numElementosActuales_componentes = elementos.length;
+
+	generarNavegadorTablaComponentes();
+}
+
+function crearTablaComponentes() {
+
+	$("#componentes-contenido").append(`
+		<div id="main-tabla-componentes" class="w-full overflow-hidden rounded-lg shadow-xs">
+
+			<div class="w-full overflow-x-auto">
+				<table class="w-full whitespace-no-wrap">
+					<thead>
+						<tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+							<th class="px-4 py-3">ID</th>
+							<th class="px-4 py-3">Tipo</th>
+							<th class="px-4 py-3">Estado</th>
+							<th class="px-4 py-3">Fecha</th>
+							<th class="px-4 py-3">Observaciones</th>
+							<th class="px-4 py-3">Acciones</th>
+						</tr>
+					</thead>
+					<tbody id="body-tabla-componentes" class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+					</tbody>
+				</table>
+			</div>
+
+		</div>
+  `);
+
+	crearComponentes();
+}
+
+function crearInterfazComponentes(data) {
+	componentes = data;
+	paginaMax_componentes = Math.ceil(data.cantidad/10);
+
+	$('#cargando-componentes').remove();
+	$('#main-tarjetas').append(tarjeta('componentes', 'Total de componentes', componentes.cantidad));
+	$('#boton-aniadir-componentes').attr("onclick","cargarFormularioVacioComponentes()"); 
+	if (componentes.cantidad) {
+		crearTablaComponentes();
+		generarPieComponentes();
+	}
+}
+
+function getPaginaComponentes(num) {
+	pagina_componentes = num;
+	crearComponentes();
+}
+
+function generarNavegadorTablaComponentes(){
+	let html = "";
+
+	// Elimina los actuales al cambiar de tabla
+	$('.elemento-navegacion-tabla_componentes').remove();
+
+	html += `
+		<li class="elemento-navegacion-tabla_componentes">
+			<button onclick="if(pagina_componentes>1) getPaginaComponentes(pagina_componentes-1);" class="px-3 py-1 rounded-md rounded-l-lg focus:outline-none focus:shadow-outline-purple" aria-label="Previous">
+				<svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
+					<path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" fill-rule="evenodd"></path>
+				</svg>
+			</button>
+		</li>
+	`;
+
+	if(paginaMax_componentes > 5){
+
+		if(pagina_componentes != 1){
+			html += `<li class="elemento-navegacion-tabla_componentes"> 
+						<button onclick="getPaginaComponentes(1);" class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+							1 
+						</button>
+					</li>
+					`
+		}
+
+		if(pagina_componentes >= 3){
+			html += `<li class="elemento-navegacion-tabla_componentes">
+						<span class="px-3 py-1">...</span>
+					</li>
+					`
+		}
+
+		html += `<li class="elemento-navegacion-tabla_componentes">
+					<button onclick="getPaginaComponentes(${pagina_componentes});" class="px-3 py-1 text-white transition-colors duration-150 bg-purple-600 border border-r-0 border-purple-600 rounded-md focus:outline-none focus:shadow-outline-purple">
+						${pagina_componentes}
+					</button>
+				</li>
+				`
+
+		if(pagina_componentes != paginaMax_componentes){
+			if(pagina_componentes<paginaMax_componentes-1){
+				html += `<li class="elemento-navegacion-tabla_componentes">
+							<span class="px-3 py-1">...</span> 
+						</li>
+						`
+			}
+
+			html += `
+				<li class="elemento-navegacion-tabla_componentes">
+					<button onclick="getPaginaComponentes(${paginaMax_componentes});" class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+						${paginaMax_componentes}
+					</button>
+				</li>
+			`
+		}
+	}
+	else{
+
+		for (i = 1; i <= paginaMax_componentes; i++) {
+			html += `
+				<li class="elemento-navegacion-tabla_componentes">
+					<button onclick="getPaginaComponentes(${i});" 
+						${ pagina_componentes!=i ? 'class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple"' : 'class="px-3 py-1 text-white transition-colors duration-150 bg-purple-600 border border-r-0 border-purple-600 rounded-md focus:outline-none focus:shadow-outline-purple"' } >
+						${i}
+					</button>
+				</li>
+			`
+		}
+
+	}
+
+	html += `
+		<li class="elemento-navegacion-tabla_componentes">
+			<button onclick="if(pagina_componentes<paginaMax_componentes) getPaginaComponentes(pagina_componentes+1);" class="px-3 py-1 rounded-md rounded-r-lg focus:outline-none focus:shadow-outline-purple" aria-label="Next">
+				<svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
+					<path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" fill-rule="evenodd"></path>
+				</svg>
+			</button>
+		</li>
+	`;
+	
+	$('#navegacion-tablas_componentes').html(html);
+}
+
+function generarPieComponentes() {
+	$('#pie-tabla_componentes').remove();
+
+	$("#main-tabla-componentes").append(`
+		<div id="pie-tabla_componentes" class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
+			<span id="cantidad-pie_componentes" class="flex items-center col-span-3">
+				Mostrando ${numElementosActuales_componentes} de ${componentes.data.length}
+			</span>
+			<span class="col-span-2"></span>
+			<!-- Pagination -->
+			<span class="flex col-span-4 mt-2 sm:mt-auto sm:justify-end">
+				<nav aria-label="Table navigation">
+					<ul id="navegacion-tablas_componentes" class="inline-flex items-center">
+					</ul>
+				</nav>
+			</span>
+		</div>
+	  `);
+	  
+	  generarNavegadorTablaComponentes();
+}
+
+// function eliminarTransformador(id, posicion){
+// 	$.ajax({
+// 		url: `/api/transformador/${id}`,
+// 		type: 'DELETE',
+// 		success: function(){
+// 			transformadores.cantidad--;
+			
+// 			if(transformadores.cantidad%10 == 0 && pagina_transformadores!=1){
+// 				pagina_transformadores--;
+// 			}
+
+// 			transformadores.data.splice(posicion,1);
+// 			crearTransformadores();
+// 			generarPieTransformadores();
+
+// 			// Actualiza la tarjeta superior
+// 			$('#elementos-totales-tarjeta-transformadores').html(transformadores.data.length);
+// 		},
+// 		error: function(){
+// 			$('#titulo-error').html('Error al eliminar')
+// 			$('#mensaje-error').html('Ha ocurrido un error al eliminar el cable')
+// 			$('.popup').removeClass('hidden');
+
+// 			setTimeout(() => $('.popup').addClass('hidden'), 3000 )
+// 		}
+// 	});
+// }
+
+function cargarFormularioComponente(id, tipo, estado, fecha, observaciones, posicion){
+
+	$('#body-modal').html(`
+	
+	<!-- Modal title -->
+	<p
+		class="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-300"
+	>
+		Editar componente
+	</p>
+	<!-- Modal description -->
+	<div class="text-sm mt-5">
+		<label class="block text-sm my-3">
+		<span class="text-gray-700 dark:text-gray-400">Tipo</span>
+		<input id="editar-tipo"
+			class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+			placeholder=""
+		/>
+		</label>
+
+		<label class="block text-sm my-3">
+		<span class="text-gray-700 dark:text-gray-400">Estado</span>
+		<select id="editar-estado"
+			class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-select focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"
+		>
+			<option>Desconocido</option>
+			<option>Bueno</option>
+			<option>Regular</option>
+			<option>Por revisar</option>
+			<option>No aprovechable</option>
+			<option>Roto</option>
+		</select>
+		</label>
+
+		<label class="block text-sm my-3">
+		<span class="text-gray-700 dark:text-gray-400">Fecha</span>
+		<input id="editar-fecha"
+			class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+			placeholder="Formato: 2000-01-31"
+		/>
+		</label>
+
+		<label class="block text-sm my-3">
+		<span class="text-gray-700 dark:text-gray-400">Observaciones</span>
+		<input id="editar-observaciones"
+			class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+			placeholder=""
+		/>
+		</label>
+	</div>
+
+	`)
+
+	$('#editar-tipo').val(tipo);
+	$(`#editar-estado`).val(estado);
+	$('#editar-fecha').val(fecha);
+	$('#editar-observaciones').val(observaciones);
+	cargarCaracteristicas(id, posicion);
+	$('#confirmar-modal').attr('onclick', `editarComponente(${id}, ${posicion})`);
+	$('#boton-nueva-caracteristica').attr('onclick', `aniadirCaracteristica({id: -1, nombre: '', valor: ''}, ${posicion}, null, ${id}, 'crear');`);
+
+}
+
+function cargarFormularioVacioComponentes(){
+
+	$('#body-modal').html(`
+	
+	<!-- Modal title -->
+	<p
+		class="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-300"
+	>
+		Editar componente
+	</p>
+	<!-- Modal description -->
+	<div class="text-sm mt-5">
+		<label class="block text-sm my-3">
+		<span class="text-gray-700 dark:text-gray-400">Tipo</span>
+		<input id="editar-tipo"
+			class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+			placeholder=""
+		/>
+		</label>
+
+		<label class="block text-sm my-3">
+		<span class="text-gray-700 dark:text-gray-400">Estado</span>
+		<select id="editar-estado"
+			class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-select focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"
+		>
+			<option>Desconocido</option>
+			<option>Bueno</option>
+			<option>Regular</option>
+			<option>Por revisar</option>
+			<option>No aprovechable</option>
+			<option>Roto</option>
+		</select>
+		</label>
+
+		<label class="block text-sm my-3">
+		<span class="text-gray-700 dark:text-gray-400">Fecha</span>
+		<input id="editar-fecha"
+			class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+			placeholder="Formato: 2000-01-31"
+		/>
+		</label>
+
+		<label class="block text-sm my-3">
+		<span class="text-gray-700 dark:text-gray-400">Observaciones</span>
+		<input id="editar-observaciones"
+			class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+			placeholder=""
+		/>
+		</label>
+	</div>
+
+	`)
+
+	$('#editar-tipo').val("");
+	$(`#editar-estado`).val("Desconocido");
+	$('#editar-fecha').val("");
+	$('#body-tabla-caracteristicas').html('');
+	$('#editar-observaciones').val("");
+	$('#confirmar-modal').attr('onclick', `crearComponente()`);
+	$('#boton-nueva-caracteristica').attr('onclick', `aniadirCaracteristicaNueva(0)`);
+}
+
+// function editarTransformador(id, posicion){
+
+// 	let voltaje  = $('#editar-voltaje').val();
+// 	let amperaje = $('#editar-amperaje').val();
+// 	let c_tipo 	 = $('#corresponde-tipo').val().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+// 	let c_id	 = $('#corresponde-id').val();	
+
+// 	let json = {
+// 		voltaje: voltaje, 
+// 		amperaje: amperaje
+// 	}
+
+// 	if(c_id){
+// 		json['corresponde'] = {
+// 			tipo: c_tipo,
+// 			id: c_id
+// 		}
+// 	}
+
+// 	$.ajax({
+// 		url: `/api/transformador/${id}`,
+// 		type: 'PUT',
+// 		data: JSON.stringify(json),
+// 		contentType: "application/json; charset=utf-8",
+// 		success: function(){
+// 			$(`#transformador_voltaje-${id}`).html(voltaje);
+// 			$(`#transformador_amperaje-${id}`).html(amperaje);
+// 			$(`#transformador_editar-${id}`).attr('onclick', `cargarFormularioTransformador('${id}', '${voltaje}', '${amperaje}', ${posicion})`);
+
+// 			transformadores.data[posicion].voltaje  = voltaje;
+// 			transformadores.data[posicion].amperaje = amperaje;
+// 			transformadores.data[posicion].corresponde = json.corresponde;
+
+// 			crearTransformadores();
+// 		},
+// 		error: function(){
+// 			$('#titulo-error').html('Error al editar el transformador')
+// 			$('#mensaje-error').html('Ha ocurrido un error al editar el transformador')
+// 			$('.popup').removeClass('hidden');
+
+// 			setTimeout(() => $('.popup').addClass('hidden'), 3000 )
+// 		}
+// 	});
+
+
+// }
+
+// function crearTransformador(){
+
+// 	let voltaje  = $('#editar-voltaje').val();
+// 	let amperaje = $('#editar-amperaje').val();
+// 	let c_tipo 	 = $('#corresponde-tipo').val().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+// 	let c_id	 = $('#corresponde-id').val();
+	
+// 	let json = {
+// 		voltaje: voltaje, 
+// 		amperaje: amperaje
+// 	}
+
+// 	if(c_id){
+// 		json['corresponde'] = {
+// 			tipo: c_tipo,
+// 			id: c_id
+// 		}
+// 	}
+	
+// 	$.ajax({
+// 		url: `/api/recogida/${recogida.id}/transformador/`,
+// 		type: 'POST',
+// 		data: JSON.stringify(json),
+// 		contentType: "application/json; charset=utf-8",
+// 		dataType: "json",
+// 		success: function(data){
+// 			let id = data.id;
+// 			transformadores.cantidad++;
+
+// 			let response_json = {
+// 				id: id,
+// 				voltaje: voltaje,
+// 				amperaje: amperaje
+// 			}
+
+// 			if(json.corresponde)
+// 				response_json.corresponde = json.corresponde;
+// 			else
+// 				response_json.corresponde = null
+
+// 			transformadores.data.push(response_json);
+
+// 			$('#elementos-totales-tarjeta-transformadores').html(transformadores.cantidad);
+
+// 			if(!$('#main-tabla-transformadores').length){
+// 				crearTablaTransformadores();
+// 				generarPieTransformadores();
+// 			}
+// 			else
+// 				crearTransformadores();
+// 		},
+// 		error: function(){
+// 			$('#titulo-error').html('Error al crear el transformador')
+// 			$('#mensaje-error').html('Ha ocurrido un error al crear el transformador')
+// 			$('.popup').removeClass('hidden');
+
+// 			setTimeout(() => $('.popup').addClass('hidden'), 3000 );
+// 		}
+// 	});
+
+// }
